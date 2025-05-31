b05170d7d428c1ce67fa372831d1d6d49237bb80
Will <git@willbradley.dev>
2025-06-01 08:05:29 +1200

    Add another example

--- src/models.ts
+++ src/models.ts
@@ -1,13 +1,21 @@
 import z from "zod/v4";
 
+export const TestId = z.string().meta({
+  id: "TestId",
+  type: "prefixid",
+  examples: ["test_12345"],
+}) as z.ZodType<`test_${string}`>;
+export type TestId = z.infer<typeof TestId>;
+
 export const Who = z
   .string()
   .regex(/^[a-zA-Z]+$/, "Who must be a string with only letters")
-  .meta({ id: "Who", type: "whoid", examples: ["World"] });
+  .meta({ id: "Who", examples: ["World"] });
 export type Who = z.infer<typeof Who>;
 
 export const MySpecialPayload = z
   .object({
+    _id: TestId.optional(),
     who: Who,
   })
   .meta({
@@ -17,6 +25,7 @@ export type MySpecialPayload = z.infer<typeof MySpecialPayload>;
 
 export const MySpecialResponse = z
   .object({
+    _id: TestId.optional(),
     who: Who,
     message: z.string(),
   })
